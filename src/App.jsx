c3877774f4ad121530b7c6c0831bf13b80a31a03
jayad23 <jaidervanegas@live.com>
2022-10-25 21:35:02 +0200

    Class 1 Week 2 Types of components and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-import Welcome from './components/welcome/Welcome'
+import React, { Fragment } from 'react'
+import Welcome from "./components/welcome/Welcome"
+import ClassComponent from './components/week_2/class_1/ClassComponent'
+import FunctionalComponent from './components/week_2/class_1/FunctionalComponent'
+import MockComponent from './components/week_2/class_1/MockComponent'
 
 const App = () => {
-    return <Welcome />
+    const greeting = {
+        hello: "Soy componente de clase",
+        subText: "Soy el hijo de Class Component"
+    }
+    return (
+        <Fragment>
+            <ClassComponent saludo={greeting} />
+            <FunctionalComponent />
+            <MockComponent message="Soy hijo de App, pero no soy el mismo que el hijo de class" />
+        </Fragment>
+    )
 }
 
 export default App
