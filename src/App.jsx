4f9b34082971ffdfc39ff86a27791ddbe83bf23a
jayad23 <jaidervanegas@live.com>
2022-10-27 21:29:17 +0200

    Semana 2, clase 2. Inmmutability, Keys, and methods

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import React, { Fragment } from 'react'
-import Welcome from "./components/welcome/Welcome"
-import ClassComponent from './components/week_2/class_1/ClassComponent'
-import FunctionalComponent from './components/week_2/class_1/FunctionalComponent'
-import MockComponent from './components/week_2/class_1/MockComponent'
+import Keys from './components/week_2/class_2/Keys'
 
 const App = () => {
     const greeting = {
         hello: "Soy componente de clase",
         subText: "Soy el hijo de Class Component"
     }
-    return (
-        <Fragment>
-            <ClassComponent saludo={greeting} />
-            <FunctionalComponent />
-            <MockComponent message="Soy hijo de App, pero no soy el mismo que el hijo de class" />
-        </Fragment>
-    )
+    return <Keys />
 }
 
 export default App
