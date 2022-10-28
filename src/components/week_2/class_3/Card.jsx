cfc51431cf1cad967e097cb7cfa14b42ced7bef0
jayad23 <jaidervanegas@live.com>
2022-10-29 00:15:45 +0200

    Solución de actividad de las Mesas.

--- src/components/week_2/class_3/Card.jsx
+++ src/components/week_2/class_3/Card.jsx
@@ -1,16 +1,57 @@
 import React, { Fragment } from 'react'
 import PowerList from './PowerList'
 
-const Card = ({ data }) => {
+const CardBackgroundColor = (type) => {
+    switch (type)
+    {
+        case "fire":
+            return {
+                borderColor: "#5A0E1A",
+                bgColor: "#B3001B",
+            }
+        case "water":
+            return {
+                borderColor: "#255C99",
+                bgColor: "#7EA3CC",
+            }
+
+        case "rock":
+            return {
+                borderColor: "#262626",
+                bgColor: "#C6C6C6",
+            }
+
+        default:
+            return;
+    }
+}
+
+const GetColors = (type) => {
+    const { borderColor, bgColor } = CardBackgroundColor(type)
+    const styles = {
+        border: `3px solid ${ borderColor }`,
+        display: "flex",
+        justifyContent: "space-evenly",
+        flexDirection: "column",
+        padding: "20px",
+        width: "200px",
+        backgroundColor: `${ bgColor }`,
+        borderRadius: "20px"
+    }
+
+    return styles;
+}
+
+const Card = ({ data, type }) => {
 
     return (
         <Fragment>
             {
                 data.map(pokemon => (
 
-                    <div key={pokemon.id} style={{ border: "1px solid red", display: "flex", justifyContent: "center", flexDirection: "column", textAlign: "center", padding: "20px" }}>
+                    <div key={pokemon.id} style={GetColors(type)}>
                         <h2>{pokemon.pokemonName}</h2>
-                        <div style={{ width: "120px" }}>
+                        <div style={{ width: "120px", margin: "0 auto" }}>
                             <img style={{ width: "100%" }} src={pokemon.avatar} alt={pokemon.name} />
                         </div>
                         {
@@ -21,7 +62,7 @@ const Card = ({ data }) => {
                     </div>
                 ))
             }
-        </Fragment>
+        </Fragment >
     )
 }
 
