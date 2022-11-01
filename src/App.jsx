5672d029a3ced8a934b63abe91399cf20ebeef83
jayad23 <jaidervanegas@live.com>
2022-11-01 19:41:56 +0100

    fils for week_3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react'
 import Keys from './components/week_2/class_2/Keys'
 import Main from './components/week_2/class_3/Main'
+import LocalApp from './components/week_3/LocalApp'
 
 const App = () => {
-    return <Main />
+    return <LocalApp />
 }
 
 export default App
---
+++ src/components/week_3/LocalApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LocalApp = () => {
+    return (
+        <div>LocalApp</div>
+    )
+}
+
+export default LocalApp
