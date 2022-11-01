21aa00d4b1a2d23ac3c30453a643b35ae17933bb
jayad23 <jaidervanegas@live.com>
2022-11-01 20:40:54 +0100

    clase 1, formas de declarar y acceder al estado

--- src/components/week_3/class_1/Counter.jsx
+++ src/components/week_3/class_1/Counter.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import style from "./styles/style.module.css";
 
-const Counter = () => {
+const Counter = ({ title, counterFn, handleIncrementFn }) => {
+    const [count, setCount] = useState(0)
+
+    const handleIncrement = () => {
+        setCount(prevState => prevState + 1)
+    }
+
     return (
-        <div>Counter</div>
+        <div className={style['counterContainer']}>
+            <button onClick={handleIncrementFn}>Incrementar</button>
+            <h1>{title} es: {counterFn}</h1>
+        </div>
     )
 }
 
