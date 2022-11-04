748ed64437e793c820efcd2f486f86441b74bc50
jayad23 <jaidervanegas@live.com>
2022-11-04 19:22:54 +0100

    archivos necesarios para la clase

---
+++ src/components/week_3/class_3/styles/styled.js
@@ -0,0 +1,65 @@
+
+/**
+ * ANCHOR
+ * ParentComonent 
+ *     width: 350px;
+    height: auto;
+    margin: 0 auto;
+    border: 1px solid gray;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 20px;
+    font-family: 'Poppins', sans-serif;
+ */
+
+// export const LogoContainer = styled.div`
+//     width: 100px;
+// `
+
+// export const Image = styled.img`
+//     width: 100%;
+// `
+// export const Form = styled.form`
+//     width: 100%;
+//     height: auto;
+// `
+
+/**
+ * Input
+ * padding: 10px;
+    width: 94%;
+    font-family: 'Poppins', sans-serif;
+    margin-top: 10px;
+    border-radius: 5px;
+    border: 1px dotted gray;
+    color: #000;
+ */
+
+/**
+ * ButtonContainer
+ * width: 100%;
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 10px;
+ */
+
+/**
+ * ButtonContainer
+ * width: 45%;
+    font-family: 'Poppins', sans-serif;
+    background-color: "#7367f0";
+    box-shadow: 0 3px 6px 0 rgb(115 103 240 / 40%);
+    color: #eee;
+    border: none;
+    border-radius: 5px;
+    font-size: .9em;
+    letter-spacing: 1px;
+    padding: 10px;
+    font-family: 'Poppins', sans-serif;
+    cursor: pointer;
+ */
