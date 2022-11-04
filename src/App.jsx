80bb5f2f64b18b4a3699b9c637ad2b007dc6e5b4
jayad23 <jaidervanegas@live.com>
2022-11-04 19:23:55 +0100

    último commit para clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React, { Fragment } from 'react'
-import Keys from './components/week_2/class_2/Keys'
-import Main from './components/week_2/class_3/Main'
-import LocalApp from './components/week_3/LocalApp'
-import FormComponent from './components/week_3/class_2/FormComponent'
+// import Keys from './components/week_2/class_2/Keys'
+// import Main from './components/week_2/class_3/Main'
+// import LocalApp from './components/week_3/LocalApp'
+// import FormComponent from './components/week_3/class_2/FormComponent'
 
 const App = () => {
-    return <FormComponent />
+    return <h1>Clase práctica 9</h1>
 }
 
 export default App
