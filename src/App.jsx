1f0cd58aec0babb866b21c363d71913983b3caac
jayad23 <jaidervanegas@live.com>
2022-11-15 20:40:08 +0100

    Métodos de ciclo de vida - Componentes de clase.

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,21 @@ import React, { Fragment, useState } from 'react'
 // import FormComponent from './components/week_3/class_2/FormComponent'
 //ACTIVIDAD DE REPASO -- TODOS => import Dashboard from './components/week_4/class_1/Dashboard'
 //import Store from './components/week_4/class_3/Store'
+import ClassComponent from './components/week_5/class_1/ClassComponent'
 
 const App = () => {
+    const [mostrar, setMostrar] = useState(false);
     return (
         <Fragment>
             <h1>Semana 5, class 1</h1>
+            <div style={{ border: "5px solid green" }}>
+                {
+                    mostrar && (
+                        <ClassComponent />
+                    )
+                }
+            </div>
+            <button onClick={() => setMostrar(!mostrar)}>Mostrar componente</button>
         </Fragment>
     )
 }
--- src/components/week_5/class_1/ClassComponent.jsx
+++ src/components/week_5/class_1/ClassComponent.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
-
+const names = ["Pedro", "Juan", "Camilo"];
 class ClassComponent extends Component {
+
+  state = {
+    index: 0,
+    message: "",
+    name: null
+  }
+
+  incrementCount = () => {
+    this.setState({ index: this.state.index + 1 });
+  }
+
+  //1ER MÉTODO DE CICLO DE VIDA - AL MONTARSE EL COMPONENTE
+  componentDidMount() {
+    this.setState({ message: "Saludos a la camada 3", name: names[this.state.index] })
+    //LLAMADOS INICIALES A API'S.
+  }
+
+  //2DO MÉTODO DE CICLO DE VIDA - AL ACTUALIZARSE EL COMPONENTE
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.index !== this.state.index) {
+      this.setState({ name: names[(this.state.index + 1) % names.length] })
+    }
+  }
+
   render() {
+    //console.log("count", this.state.index);
     return (
       <div>
-        
+        <h1>El nombre es: {this.state.name}</h1>
+        <p>La cuenta va en: {this.state.index}</p>
+        <p>El mensaje es: {this.state.message}</p>
+        <button onClick={() => this.incrementCount()}>Incrementar Index</button>
       </div>
     );
   }
